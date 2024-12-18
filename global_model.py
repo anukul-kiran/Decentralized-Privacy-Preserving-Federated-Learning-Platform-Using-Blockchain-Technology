189e2ebffc7ac55342f5ff6c7f8fb711f0e64738
anukul-kiran <balasubramanian120@gmail.com>
2024-12-18 20:35:57 +0530

    Created Synthetic Dataset

---
+++ global_model.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+class GlobalModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(64, 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 128)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+
